ae2fd7197ef82f90876bac62b24d94a9ccee0fe8
NEWBOY\Roberto <robi.maresca@gmail.com>
2023-04-14 01:14:21 +0200

    Changed json directory to /tmp

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,11 +1,7 @@
 const fs = require('fs');
-const path = require('path');
 
 export function writeOtp(updates) {
-    //Find the absolute path of the json directory
-    const jsonDirectory = path.join(process.cwd(), 'json');
-  
-    fs.readFile(jsonDirectory + '/otp.json', 'utf8', function (err, data) {
+    fs.readFile('/tmp/otp.json', 'utf8', function (err, data) {
         if (err) {
             console.error(err);
             return;
@@ -17,10 +13,11 @@ export function writeOtp(updates) {
             json[key] = updates[key];
         }
 
-        fs.writeFile(jsonDirectory + '/otp.json', JSON.stringify(json, null, 2), 'utf8', function (err) {
+        fs.writeFile('/tmp/otp.json', JSON.stringify(json, null, 2), 'utf8', function (err) {
             if (err) {
                 console.error(err);
             }
         });
+
     });
 }
