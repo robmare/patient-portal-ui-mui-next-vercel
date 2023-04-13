ba34c2d2c954ca92ff71302b8a9dc2c2419600ac
NEWBOY\Roberto <robi.maresca@gmail.com>
2023-04-14 01:26:39 +0200

    Checked if otp.json exists

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -1,8 +1,12 @@
 import { promises as fs } from 'fs';
 
 export default async function getOtp(req, res) {
-  //Read the json data file data.json
-  const fileContents = await fs.readFile('/tmp/otp.json', 'utf8');
-  //Return the content of the data file in json format
-  res.status(200).json(JSON.parse(fileContents));
+  if (fs.existsSync('/tmp/otp.json')) {
+    //Read the json data file data.json
+    const fileContents = await fs.readFile('/tmp/otp.json', 'utf8');
+    //Return the content of the data file in json format
+    res.status(200).json(JSON.parse(fileContents));
+  } else {
+    res.status(200).json(JSON.parse('{ "otp": "none" }'));
+  }
 }
